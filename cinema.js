340b4463e2d3b535aa1cfc28c2578d8f6a93cc42
Evandro <evandro.souzac@gmail.com>
2022-08-14 21:24:16 -0300

    added new functions

--- cinema.js
+++ cinema.js
@@ -20,7 +20,7 @@ function adicionarFilme(
   };
   catalogo.push(novoFilme);
 
-  fs.writeFile("./database/catalogo.json", JSON.stringify(catalogo, null, 3), err => {
+  fs.writeFile("./database/catalogo.json", JSON.stringify(catalogo, null, '\t'), err => {
     if (err) throw err; 
     console.log("Filme adicionado!")
   })
@@ -31,19 +31,41 @@ function buscarFilme(id) {
     console.log(`O filme escolhido foi ${filmeBuscado.titulo}. Estrelando: ${filmeBuscado.atores}. Ele tem ${filmeBuscado.duracao} minutos de duração e foi lançado em ${filmeBuscado.anoDeLancamento}`)
 }
 
-
 function alterarStatusEmCartaz(id) {
   let filmeAlterado = catalogo.find(filme => filme.codigo === id) 
-    if (filmeAlterado.emCartaz == true) {
-      filmeAlterado.emCartaz = false
-    } else {
-      filmeAlterado.emCartaz = true
-    }
-    
-    fs.writeFile("./database/catalogo.json", JSON.stringify(catalogo, null, 3), err => {
+    filmeAlterado.emCartaz == true ? 
+    filmeAlterado.emCartaz = false : 
+    filmeAlterado.emCartaz = true
+    fs.writeFile("./database/catalogo.json", JSON.stringify(catalogo, null, '\t'), err => {
       if (err) throw err; 
       console.log("Filme em cartaz foi alterado!")
     }) 
 }
 
-alterarStatusEmCartaz(45)
+function listarTodosOsFilmes() {
+  catalogo.forEach((filme, index) => {
+    console.log(`Filme ${index + 1}:
+    ID: ${filme.codigo}
+    Titulo: ${filme.titulo}
+    Duração: ${filme.duracao} minutos
+    Atores: ${filme.atores}
+    Ano de Lançamento: ${filme.anoDeLancamento}\n`)
+  })
+}
+
+function listarFilmesEmCartaz() {
+  let filmeEmCartaz = catalogo.find(filme => filme.emCartaz == true)
+    console.log(`Os filmes em cartaz no momento são:
+    ID: ${filmeEmCartaz.codigo}
+    Titulo: ${filmeEmCartaz.titulo}
+    Duração: ${filmeEmCartaz.duracao} minutos
+    Atores: ${filmeEmCartaz.atores}
+    Ano de Lançamento: ${filmeEmCartaz.anoDeLancamento}\n`)
+}
+
+
+// adicionarFilme()
+// buscarFilme()
+// alterarStatusEmCartaz(45)
+// listarTodosOsFilmes()
+// listarFilmesEmCartaz()
